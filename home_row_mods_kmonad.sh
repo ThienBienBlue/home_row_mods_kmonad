7d3f71027a27097ef6a56be91876199f12d1dffb
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2023-09-25 20:53:52 -0700

    List all the found keyboards prior to enabling mods.
    This should help with future debugging.

--- home_row_mods_kmonad.sh
+++ home_row_mods_kmonad.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-[[ -d /dev/input/by-id ]] && my_keyboard=$(find /dev/input/by-id -print | grep -E '(kbd|usb-Logitech_USB_Receiver-if02-event-mouse)')
-[[ -z "$my_keyboard" ]] && my_keyboard=$(find /dev/input/by-path -name '*kbd*' -print)
+[[ -d /dev/input/by-id ]] && keyboards=$(find /dev/input/by-id -print | grep -E '(kbd|usb-Logitech_USB_Receiver-if02-event-mouse)')
+[[ -z "$keyboards" ]] && keyboards=$(find /dev/input/by-path -name '*kbd*' -print)
 
-for keyboard in $my_keyboard; do
+echo "Found keyboards $keyboards"
+for keyboard in $keyboards; do
     export KEYBOARD=$keyboard
-    echo "Enabling home row mods for keyboard $keyboard"
+    echo "Enabling home row mods for $keyboard"
     kmonad <(envsubst < "$HOME/.config/home_row_mods_kmonad/home_row_mods.kbd")
 done
