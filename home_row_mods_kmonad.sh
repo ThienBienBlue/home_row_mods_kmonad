4e1ce521ffba45d756b541d38740754b9621c43b
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2023-09-29 20:39:08 -0700

    Shell script me harder
    Lays out all keyboard matches and lets user select from a list by index.

--- home_row_mods_kmonad.sh
+++ home_row_mods_kmonad.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-[[ -d /dev/input/by-id ]] && keyboards=$(find /dev/input/by-id -print | grep -E '(kbd|usb-Logitech_USB_Receiver-if02-event-mouse)')
-[[ -z "$keyboards" ]] && keyboards=$(find /dev/input/by-path -name '*kbd*' -print)
+[[ -d /dev/input/by-id ]] && keyboards="$(find /dev/input/by-id -print | grep -E '(kbd|usb-Logitech_USB_Receiver-if02-event-mouse)') "
+keyboards+=$(find /dev/input/by-path -name '*kbd*' -print)
 
-echo "Found keyboards $keyboards"
-for keyboard in $keyboards; do
-    export KEYBOARD=$keyboard
-    echo "Enabling home row mods for $keyboard"
-    kmonad <(envsubst < "$HOME/.config/home_row_mods_kmonad/home_row_mods.kbd")
-done
+if [[ -n "$keyboards" ]]; then
+    readarray -t keyboards < <(echo "$keyboards")
+    i=0
+
+    echo "Please select one of the following keyboards to apply home row mods to:"
+    echo
+    for keyboard in $keyboards; do
+        i=$((i + 1))
+        echo "[$i] $keyboard"
+    done
+
+    echo
+    read -p "Select one of [1-$i]: " selected_idx
+    i=1
+    for keyboard in $keyboards; do
+        if [[ "$i" == "$selected_idx" ]]; then
+            echo "Enabling home row mods for keyboard: $keyboard"
+            export KEYBOARD=$keyboard
+            kmonad <(envsubst < "$HOME/.config/home_row_mods_kmonad/home_row_mods.kbd")
+            exit 0
+        fi
+        i=$((i+1))
+    done
+
+    echo "Unrecognized input \`$selected_idx'"
+    exit 1
+fi
